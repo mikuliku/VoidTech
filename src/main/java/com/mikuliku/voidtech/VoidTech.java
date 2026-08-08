69367a80ee4e2995cdb108de7e77ef1ce22f1439
mikuliku <rk20090425@qq.com>
2026-08-08 22:01:25 +0800

    Create VoidTech.java

---
+++ src/main/java/com/mikuliku/voidtech/VoidTech.java
@@ -0,0 +1,12 @@
+package com.mikuliku.voidtech;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(VoidTech.MOD_ID)
+public final class VoidTech {
+    public static final String MOD_ID = "voidtech";
+
+    public VoidTech() {
+        // Registration and gameplay systems are expanded in subsequent development.
+    }
+}
