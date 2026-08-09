a4346f8e108906cd72e3c3372035b2dac9c91b27
mikuliku <rk20090425@qq.com>
2026-08-09 13:22:52 +0800

    Update VoidTech.java

--- src/main/java/com/mikuliku/voidtech/VoidTech.java
+++ src/main/java/com/mikuliku/voidtech/VoidTech.java
@@ -2,6 +2,7 @@ package com.mikuliku.voidtech;
 
 
 import com.mikuliku.voidtech.registry.ModBlocks;
+import com.mikuliku.voidtech.registry.ModMenuTypes;
 import com.mikuliku.voidtech.registry.ModMenus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
