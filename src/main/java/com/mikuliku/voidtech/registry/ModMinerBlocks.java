8658c5f1894717cfa5ebc8e19c0c47e71057607a
mikuliku <rk20090425@qq.com>
2026-08-09 14:14:04 +0800

    Update ModMinerBlocks.java

--- src/main/java/com/mikuliku/voidtech/registry/ModMinerBlocks.java
+++ src/main/java/com/mikuliku/voidtech/registry/ModMinerBlocks.java
@@ -1,19 +1,24 @@
 package com.mikuliku.voidtech.registry;
 
+
 import com.mikuliku.voidtech.VoidTech;
+
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
+
 public class ModMinerBlocks {
+
 
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(
                     ForgeRegistries.BLOCKS,
-                    VoidTech.MODID
+                    VoidTech.MOD_ID
             );
 
 
