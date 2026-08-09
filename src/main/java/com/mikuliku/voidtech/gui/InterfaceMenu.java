0cbbff034d307685edc31e256715033e5d90bdf2
mikuliku <rk20090425@qq.com>
2026-08-09 14:07:55 +0800

    Refactor InterfaceMenu constructor and method signature

--- src/main/java/com/mikuliku/voidtech/gui/InterfaceMenu.java
+++ src/main/java/com/mikuliku/voidtech/gui/InterfaceMenu.java
@@ -1,20 +1,35 @@
 package com.mikuliku.voidtech.gui;
 
+
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+
 import net.minecraft.world.inventory.AbstractContainerMenu;
-import net.minecraft.world.inventory.MenuType;
+
+
 
 public class InterfaceMenu extends AbstractContainerMenu {
 
 
-    public InterfaceMenu(int id, Inventory inventory, FriendlyByteBuf data) {
-        super(null, id);
+
+    public InterfaceMenu(
+            int id,
+            Inventory inventory,
+            FriendlyByteBuf data
+    ){
+
+        super(null,id);
+
     }
+
 
 
     @Override
-    public boolean stillValid(net.minecraft.world.entity.player.Player player) {
+    public boolean stillValid(Player player){
+
         return true;
+
     }
+
 }
