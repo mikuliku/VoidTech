d1ed35427e204622608973c93a267b39a37035be
mikuliku <rk20090425@qq.com>
2026-08-09 14:11:08 +0800

    Update VoidMinerMenu.java

--- src/main/java/com/mikuliku/voidtech/gui/VoidMinerMenu.java
+++ src/main/java/com/mikuliku/voidtech/gui/VoidMinerMenu.java
@@ -4,9 +4,7 @@ package com.mikuliku.voidtech.gui;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
-
 import net.minecraft.world.inventory.AbstractContainerMenu;
-
 import net.minecraft.world.item.ItemStack;
 
 
@@ -14,15 +12,12 @@ import net.minecraft.world.item.ItemStack;
 public class VoidMinerMenu extends AbstractContainerMenu {
 
 
-
     public VoidMinerMenu(
             int id,
             Inventory inventory,
             FriendlyByteBuf data
     ){
-
         super(null,id);
-
     }
 
 
