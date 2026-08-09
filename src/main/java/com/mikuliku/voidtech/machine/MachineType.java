6a8938182d83eda8c44dcd553e15d6ee252c735c
mikuliku <rk20090425@qq.com>
2026-08-09 22:56:05 +0800

    Create MachineType.java

---
+++ src/main/java/com/mikuliku/voidtech/machine/MachineType.java
@@ -0,0 +1,20 @@
+package com.mikuliku.voidtech.machine;
+
+
+
+public enum MachineType {
+
+
+    /**
+     * 虚空采矿机
+     */
+    VOID_MINER,
+
+
+    /**
+     * 虚空流体生成机
+     */
+    VOID_FLUID_GENERATOR
+
+
+}
