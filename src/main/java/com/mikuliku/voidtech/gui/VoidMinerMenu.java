2436cba9d40b17da3b0a90eb1bc1c92bec30e630
mikuliku <rk20090425@qq.com>
2026-08-09 13:24:17 +0800

    Refactor VoidMinerMenu constructor

--- src/main/java/com/mikuliku/voidtech/gui/VoidMinerMenu.java
+++ src/main/java/com/mikuliku/voidtech/gui/VoidMinerMenu.java
@@ -8,18 +8,17 @@ import net.minecraft.world.item.ItemStack;
 
 public class VoidMinerMenu extends AbstractContainerMenu {
 
+
     public VoidMinerMenu(int id, Inventory inventory) {
-        super(null, id);
+        super(null,id);
     }
 
-    public VoidMinerMenu(MenuType<?> type, int id, Inventory inventory) {
-        super(type, id);
-    }
 
     @Override
     public boolean stillValid(Player player) {
         return true;
     }
+
 
     @Override
     public ItemStack quickMoveStack(Player player, int index) {
