7aa9ad2bd1a34dab64d582006771279a11ec6528
mikuliku <rk20090425@qq.com>
2026-08-09 22:24:08 +0800

    Update VoidTech.java

--- src/main/java/com/mikuliku/voidtech/VoidTech.java
+++ src/main/java/com/mikuliku/voidtech/VoidTech.java
@@ -2,11 +2,13 @@ package com.mikuliku.voidtech;
 
 
 import com.mikuliku.voidtech.registry.ModBlocks;
-import com.mikuliku.voidtech.registry.ModMinerBlocks;
-import com.mikuliku.voidtech.registry.ModMenuTypes;
 import com.mikuliku.voidtech.registry.ModItems;
+
+
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
 
 
 @Mod(VoidTech.MOD_ID)
@@ -16,17 +18,21 @@ public class VoidTech {
     public static final String MOD_ID = "voidtech";
 
 
-    public VoidTech() {
+    public VoidTech(){
 
-        var bus =
+
+        IEventBus bus =
                 FMLJavaModLoadingContext
                         .get()
                         .getModEventBus();
 
 
+
         ModBlocks.BLOCKS.register(bus);
+
         ModItems.ITEMS.register(bus);
-        ModMenuTypes.MENUS.register(bus);
+
 
     }
+
 }
