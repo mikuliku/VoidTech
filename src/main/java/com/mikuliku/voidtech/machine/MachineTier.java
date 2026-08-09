c4f1143a2670d66a1a1bb39b7e5e8c71f8e4b6c8
mikuliku <rk20090425@qq.com>
2026-08-09 22:42:22 +0800

    Update MachineTier.java

--- src/main/java/com/mikuliku/voidtech/machine/MachineTier.java
+++ src/main/java/com/mikuliku/voidtech/machine/MachineTier.java
@@ -3,82 +3,159 @@ package com.mikuliku.voidtech.machine;
 
 public enum MachineTier {
 
+
     TIER_1(
-            "虚空采矿机 I",
+            1,
+            "Void Miner I",
             100000,
-            1
+            1,
+            1,
+            2,
+            "voidtech:void_frame"
     ),
+
 
     TIER_2(
-            "虚空采矿机 II",
+            2,
+            "Void Miner II",
             500000,
-            2
+            2,
+            2,
+            3,
+            "voidtech:reinforced_void_frame"
     ),
+
 
     TIER_3(
-            "虚空采矿机 III",
-            2000000,
-            3
+            3,
+            "Void Miner III",
+            2500000,
+            4,
+            3,
+            4,
+            "voidtech:advanced_void_frame"
     ),
+
 
     TIER_4(
-            "虚空采矿机 IV",
+            4,
+            "Void Miner IV",
             10000000,
-            4
+            8,
+            5,
+            5,
+            "voidtech:quantum_void_frame"
     ),
+
 
     TIER_5(
-            "虚空采矿机 V",
+            5,
+            "Void Miner V",
             50000000,
-            5
+            15,
+            8,
+            6,
+            "voidtech:stellar_void_frame"
     ),
 
+
     TIER_6(
-            "虚空采矿机 VI",
+            6,
+            "Void Miner VI",
             250000000,
-            6
+            30,
+            12,
+            8,
+            "voidtech:infinity_void_frame"
     );
 
 
-    private final String name;
-
-    private final int energyCapacity;
 
     private final int level;
 
+    private final String displayName;
+
+    private final int energyCapacity;
+
+    private final int outputMultiplier;
+
+    private final int speedMultiplier;
+
+    private final int upgradeSlots;
+
+    private final String frameMaterial;
+
+
 
     MachineTier(
-            String name,
+            int level,
+            String displayName,
             int energyCapacity,
-            int level
+            int outputMultiplier,
+            int speedMultiplier,
+            int upgradeSlots,
+            String frameMaterial
     ){
 
-        this.name = name;
-        this.energyCapacity = energyCapacity;
         this.level = level;
+        this.displayName = displayName;
+        this.energyCapacity = energyCapacity;
+        this.outputMultiplier = outputMultiplier;
+        this.speedMultiplier = speedMultiplier;
+        this.upgradeSlots = upgradeSlots;
+        this.frameMaterial = frameMaterial;
 
     }
 
 
-    public String getName(){
 
-        return name;
+    public int getLevel(){
+        return level;
+    }
 
+
+    public String getDisplayName(){
+        return displayName;
     }
 
 
     public int getEnergyCapacity(){
-
         return energyCapacity;
-
     }
 
 
-    public int getLevel(){
-
-        return level;
-
+    public int getOutputMultiplier(){
+        return outputMultiplier;
     }
 
+
+    public int getSpeedMultiplier(){
+        return speedMultiplier;
+    }
+
+
+    public int getUpgradeSlots(){
+        return upgradeSlots;
+    }
+
+
+    public String getFrameMaterial(){
+        return frameMaterial;
+    }
+
+
+
+    public static MachineTier fromLevel(int level){
+
+        for(MachineTier tier : values()){
+
+            if(tier.level == level)
+                return tier;
+
+        }
+
+        return TIER_1;
+
+    }
 
 }
