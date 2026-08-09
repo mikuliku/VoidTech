7893c078eae367dc2f3c6c1f1e97ece81e07d8fd
mikuliku <rk20090425@qq.com>
2026-08-09 12:36:41 +0800

    Update InterfaceMenu.java

--- src/main/java/com/mikuliku/voidtech/gui/InterfaceMenu.java
+++ src/main/java/com/mikuliku/voidtech/gui/InterfaceMenu.java
@@ -3,23 +3,26 @@ package com.mikuliku.voidtech.gui;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.MenuType;
+import net.minecraft.world.item.ItemStack;
 
 public class InterfaceMenu extends AbstractContainerMenu {
-
 
     public InterfaceMenu(int id, Inventory inventory) {
         super(null, id);
     }
 
+    public InterfaceMenu(MenuType<?> type, int id, Inventory inventory) {
+        super(type, id);
+    }
 
     @Override
     public boolean stillValid(Player player) {
         return true;
     }
 
-
     @Override
-    public net.minecraft.world.item.ItemStack quickMoveStack(Player player, int index) {
-        return net.minecraft.world.item.ItemStack.EMPTY;
+    public ItemStack quickMoveStack(Player player, int index) {
+        return ItemStack.EMPTY;
     }
 }
