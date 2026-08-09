4f645ca286ce30551488c38427c94bb965fd5b71
mikuliku <rk20090425@qq.com>
2026-08-09 14:15:50 +0800

    Update ModMenuTypes.java

--- src/main/java/com/mikuliku/voidtech/registry/ModMenuTypes.java
+++ src/main/java/com/mikuliku/voidtech/registry/ModMenuTypes.java
@@ -7,9 +7,7 @@ import com.mikuliku.voidtech.gui.VoidMinerMenu;
 
 
 import net.minecraft.world.inventory.MenuType;
-
 import net.minecraftforge.common.extensions.IForgeMenuType;
-
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -30,7 +28,14 @@ public class ModMenuTypes {
     public static final RegistryObject<MenuType<VoidMinerMenu>> VOID_MINER_MENU =
             MENUS.register(
                     "void_miner_menu",
-                    () -> IForgeMenuType.create(VoidMinerMenu::new)
+                    () -> IForgeMenuType.create(
+                            (windowId, inv, data) ->
+                                    new VoidMinerMenu(
+                                            windowId,
+                                            inv,
+                                            data
+                                    )
+                    )
             );
 
 
@@ -38,7 +43,15 @@ public class ModMenuTypes {
     public static final RegistryObject<MenuType<InterfaceMenu>> INTERFACE_MENU =
             MENUS.register(
                     "interface_menu",
-                    () -> IForgeMenuType.create(InterfaceMenu::new)
+                    () -> IForgeMenuType.create(
+                            (windowId, inv, data) ->
+                                    new InterfaceMenu(
+                                            windowId,
+                                            inv,
+                                            data
+                                    )
+                    )
             );
+
 
 }
