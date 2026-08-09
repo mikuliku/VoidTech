95a3198579ae4d49ac64ce2310665ee312cf6db7
mikuliku <rk20090425@qq.com>
2026-08-09 12:30:17 +0800

    Update VoidMinerMenu.java

--- src/main/java/com/mikuliku/voidtech/gui/VoidMinerMenu.java
+++ src/main/java/com/mikuliku/voidtech/gui/VoidMinerMenu.java
@@ -1,32 +1,23 @@
 package com.mikuliku.voidtech.gui;
 
-
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.MenuType;
 
-
 public class VoidMinerMenu extends AbstractContainerMenu {
 
-
-    public VoidMinerMenu(
-            int id,
-            Inventory inventory
-    ){
-
-        super(null,id);
-
+    public VoidMinerMenu(int id, Inventory inventory) {
+        super(null, id);
     }
-
-
 
     @Override
-    public boolean stillValid(Player player){
-
+    public boolean stillValid(Player player) {
         return true;
-
     }
 
-
+    @Override
+    public net.minecraft.world.item.ItemStack quickMoveStack(Player player, int index) {
+        return net.minecraft.world.item.ItemStack.EMPTY;
+    }
 }
